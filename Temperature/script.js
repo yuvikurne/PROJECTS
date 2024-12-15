8318c239de09ca5fae42a267dca3a77d355505d1
Yuvraj <yuvrajkurne2311@gmail.com>
2024-12-15 11:49:05 +0530

    changes in temp folder

--- Temperature/script.js
+++ Temperature/script.js
@@ -1,24 +1,34 @@
-function ConvertTemperature() {
-    var inputTemp = parseFloat(document.getElementById("display").value);
-    var fromUnit = document.getElementById("degreeSelect").value;
-    var toUnit = document.getElementById("degreeSelect2").value;
-
-    var result;
-
-    if (fromUnit === "Celsius") {
-        if (toUnit === "Fahrenheit") {
-            result = (inputTemp * 9/5) + 32;
+document.getElementById('button').addEventListener('click', function(){
+    const inputTemp = (document.getElementById('input').value)
+    const inputUnit = document.getElementById('select').value
+    const convertToUnit = document.getElementById('selector').value
+    const temperatureDisplay = document.getElementById('temperature')
+    let convertedTemp;
+    if  (inputUnit === 'F') {
+        if (convertToUnit === 'Celcius') {
+            convertedTemp = (inputTemp - 32) * 5/9;
+            temperatureDisplay.innerHTML = `${convertedTemp}<span style="vertical-align: super; font-size: 30px">°C</span>`
         } else {
-            result = inputTemp; 
+            convertedTemp = inputTemp;
         }
-    } else if (fromUnit === "Fahrenheit") {
-        if (toUnit === "Celsius") {
-            result = ((inputTemp - 32) * 5) / 9;
+    }else if (inputUnit === 'C') {
+        if (convertToUnit === 'Fahrenheit') {
+            convertedTemp = (inputTemp * 9/5) + 32;
+            temperatureDisplay.innerHTML = `${convertedTemp}<span style="vertical-align: super; font-size: 30px">°F</span>`
         } else {
-            result = inputTemp; 
+            convertedTemp = inputTemp; 
         }
-    } 
+    }
+})
 
-    var resultBox = document.querySelector(".view");
-    resultBox.innerHTML = `<h3>${result.toFixed(2)}<span style="vertical-align: super;">&deg;${toUnit.charAt(0)}</span></h3>`;
-}
+document.getElementById('select').addEventListener('change', function() {
+    const inputUnit = document.getElementById('select').value;
+    const selector = document.getElementById('selector');
+
+    // Add new options based on the selected unit
+    if (inputUnit === 'C') {
+        selector.value = 'Fahrenheit';
+    } else if (inputUnit === 'F') {
+        selector.value = 'Celcius'
+    }
+});
